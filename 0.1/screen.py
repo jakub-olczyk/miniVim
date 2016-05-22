3de10eb540d9f64a1d4995843dcc3df2ed684c79
Jakub Olczyk <jakub.olczyk@openmailbox.org>
2016-05-22 14:37:35 +0200

    Enhance the bridge between screen and curses

--- 0.1/screen.py
+++ 0.1/screen.py
@@ -13,12 +13,11 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-Here is all the code that interacts with the curses library.
-It uses the Bridge pattern .
-'''
-import os, curses
 
+import os, curses
+from utils import Singleton
+
+@Singleton
 class Screen(object):
     ''' This is abstraction of the user interface. It uses the curses library for
     displaying actual stuff on the screen.'''
@@ -26,7 +25,7 @@ class Screen(object):
     def __init__(self, buffer):
         self.current_buffer = buffer
         self.stdscr = curses.initscr()
-        self.MAX_Y, self.MAX_X = self.stdscr.getmaxyx()
+        self.MAX_Y, self.MAX_X = self.getmaxyx()
         
         self.stdscr.keypad(True)
         curses.noecho()
@@ -40,24 +39,34 @@ class Screen(object):
         curses.echo()
         curses.endwin()
 
+    def draw(self, read):
+        ''' Main drawing routine '''
+        self.draw_cursor_position() # show y,x position of cursor on the screen
+        self.draw_last_pressed(read) # display last character pressed'
+        self.print_buffer() # display the contents of buffer
+        self.draw_cursor() #show cursor on the screen 
 
-    def draw_cursor_position(self):
-        '''
-        clears the screen
-        then draws the cursor position in current buffer
-        '''
-
+    def clear(self):
         self.stdscr.clear()
-        curr_line = self.current_buffer.current_line
-        curr_letter = self.current_buffer.current_letter
-        positon = '{},{}'.format(curr_line,curr_letter)
-        self.stdscr.addstr(self.MAX_Y-1, self.MAX_X-5, positon, curses.A_REVERSE)
-    
+
+    def refresh(self):
+        self.stdscr.refresh()
+
+    def getmaxyx(self):
+        return self.stdscr.getmaxyx()
+
+    def print_bar(self, message):
+        ''' Prints message in the bottom bar '''
+        curses.echo()
+        self.stdscr.addstr(self.MAX_Y - 1, 0, str(message))
+        self.refresh()
+
     def print_buffer(self):
         ''' prints the currently open buffer ''' 
 
         for i, line in enumerate(self.current_buffer):
             self.stdscr.addstr(i, 0, line)
+        self.refresh()
         # fill the screen with '~' if file is shorter then maximal lines on
         # screen
         buffer_len = len(self.current_buffer)
@@ -67,12 +76,33 @@ class Screen(object):
             for i in xrange(abs(difference)):
                 self.stdscr.addstr(len(self.current_buffer) + i, 0, '~')
 
+    def draw_cursor(self):
+        ''' Show current position of cursor '''
+        curses.curs_set(2)
+        self.stdscr.refresh()
+        curses.setsyx(self.current_buffer.current_line, self.current_buffer.current_letter)
+        curses.doupdate()
+
+    def draw_cursor_position(self):
+        '''
+        clears the screen
+        then draws the cursor position in current buffer
+        '''
+
+        self.clear()
+        curr_line = self.current_buffer.current_line
+        curr_letter = self.current_buffer.current_letter
+        positon = '{},{}'.format(curr_line,curr_letter)
+        self.stdscr.addstr(self.MAX_Y-1, self.MAX_X-5, positon, curses.A_REVERSE)
+
+    def draw_last_pressed(self, read):
+        ''' show last pressed character on the bar '''
+        if read != '\n':
+            self.stdscr.addstr(self.MAX_Y-1, self.MAX_X-20, str(read))
+    
     def normal_mode(self):
         ''' Set screen to be like normal mode in Vim '''
-        pass
-
-
-
+        curses.noecho()
 
     def option_mode(self):
         ''' Set screen to be in mode where you enter the commands on commandbar '''
@@ -80,40 +110,6 @@ class Screen(object):
         self.stdscr.addstr(self.MAX_Y-1, 2, ':')
         self.stdscr.refresh()
         curses.echo()
-        
- 
-    def draw(self, read):
-        ''' Main drawing routine '''
-        self.stdscr.clear()
-        # draw the line and letter number
-        self.draw_cursor_position()
-        # display last character pressed'
-        if read != '\n':
-            self.stdscr.addstr(self.MAX_Y-1, self.MAX_X-20, str(read))
-        # display the contents of buffer
-        self.print_buffer()
-
-        # DEBUG keys ↓
-        # print the command_stack
-        if read == 'KEY_F(2)':
-            self.stdscr.addstr(MAX_Y-1, 0, 'cmd_stack: ' + str(len(ed.command_stack))) 
-
-        # print the undo_stack
-        if read == 'KEY_F(3)':
-            self.stdscr.addstr(MAX_Y-1, 0, 'undo_stack: ' + str(len(ed.undo_stack))) 
-
-        if read == 'KEY_F(4)':
-            self.stdscr.addstr(MAX_Y-1, 0, 'main_buffer: ' + str(len(ed.main_buffer))) 
-
-        if read == 'KEY_F(5)':
-            self.stdscr.addstr(MAX_Y - 20, 10, self.current_buffer[self.current_line])
-
-        #show cursor on the screen 
-        curses.curs_set(2)
-        self.stdscr.refresh()
-        curses.setsyx(self.current_buffer.current_line, self.current_buffer.current_letter)
-        curses.doupdate()
-
 
 def insert_mode(func):
     ''' Set screen to be in insert mode then execute what needs executing '''
